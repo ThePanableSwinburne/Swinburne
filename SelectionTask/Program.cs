d003730bba86e587de3fab7fcf9828510eb28792
Dhanveer Ramnauth <dhanr1606@gmail.com>
2022-03-01 12:11:51 +1100

    First commit

---
+++ SelectionTask/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SelectionTask
+{
+    class Program
+    {
+        class LoginInfo
+        {
+            private string username;
+            private string password;
+           
+            public LoginInfo(string username, string password)
+            {
+                this.username = username;
+                this.password = password;
+            }
+            
+            public bool CheckPassword(string passwordAttempt) => passwordAttempt == password;
+            public bool CheckUsername(string usernameAttempt) => usernameAttempt.ToUpper() == username.ToUpper();
+            public string GetUsername() => username;
+
+        }
+
+        static void Main(string[] args)
+        {
+            LoginInfo user1 = new LoginInfo("Student", "applepie");
+
+            Console.WriteLine("Please enter your username: ");
+            if (user1.CheckUsername(Console.ReadLine()))
+            {
+                Console.WriteLine("Please enter your password: ");
+                if (user1.CheckPassword(Console.ReadLine()))
+                {
+                    Console.WriteLine("login successful");
+                }
+                else
+                {
+                    Console.WriteLine("login unsuccessful");
+                }
+            }
+            else
+            {
+                Console.WriteLine("login unsuccessful");
+            }
+
+
+        }
+    }
+}
---
+++ VariablesTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace VariablesTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name: ");
+            var firstName = Console.ReadLine();
+            
+            Console.WriteLine("Enter surname: ");
+            var surname = Console.ReadLine();
+            
+            Console.WriteLine("Enter StudentID: ");
+            var studentID = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter year of birth: ");
+            var birthYear = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter current year: ");
+            var currentYear = Convert.ToInt32(Console.ReadLine());
+
+            var age = currentYear - birthYear;
+            Console.WriteLine($"{firstName} {surname} \n{age}");
+
+        }
+    }
+}
